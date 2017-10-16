06a5fc1255d94fe662fa0f35eb7523c0a8df47c8
yvinkesteijn <yevinkesteijn@gmail.com>
2017-10-16 13:36:14 +0200

    edits 171016
    checks if files empty do while prompt for iPhone of android

---
+++ edit/RunAllFiles.py
@@ -0,0 +1,14 @@
+
+# TODO comment everything
+# TODO create functions that do specific parts
+# TODO create a master that calls the functions
+
+# file that you want to use
+# be sure to change this before running the script
+import glob
+allfiles = glob.glob('bingowa.txt')
+
+from Master import Master
+
+for filename in allfiles:
+    Master(filename)
